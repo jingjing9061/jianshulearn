ed14497b1a3a57e9adc1a5446d846338ca6d1dd1
June <1179012421@qq.com>
2019-11-21 14:09:05 +0800

    commit all

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {GlobalStyled} from './style.js';
+import Header from './common/header/index.js';
+
+function App() {
+  return (
+    <div className="col">
+        <GlobalStyled/>
+        <Header/>
+    </div>
+  );
+}
+
+export default App;
